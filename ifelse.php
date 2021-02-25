5527c2c97f127c73739e637e6568cbec4456ad2e
tbmnurulhamdi <tb.hamdi30@gmail,com>
2021-02-26 00:05:43 +0700

    update5

---
+++ ifelse.php
@@ -0,0 +1,21 @@
+<?php
+
+$nama = 'andi';
+$alamat ="serang";
+
+if ($nama == "andi") {
+    echo "kamu andi";
+    echo  "<br>";
+    if ($alamat == "serang") {
+        echo "kamu orang serang";
+    } else {
+        echo "kamu orang mana?";
+    }
+    
+} else {
+    echo "kamu siapa?";
+}
+
+
+
+?>
--- index.php
+++ index.php
@@ -1,20 +1,42 @@
-<?php
-
-?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
-
+	<title>Membuat Login Multi User Level Dengan PHP dan MySQLi - www.malasngoding.com</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-    
+ 
+	<h1>Membuat Login Multi User Level Dengan PHP dan MySQLi <br/> www.malasngoding.com</h1>
+
+	<?php 
+	if(isset($_GET['pesan'])){
+		if($_GET['pesan']=="gagal"){
+			echo "<div class='alert'>Username dan Password tidak sesuai !</div>";
+		}
+	}
+	?>
+ 
+	<div class="kotak_login">
+		<p class="tulisan_login">Silahkan login</p>
+ 
+		<form action="cek_login.php" method="post">
+			<label>Username</label>
+			<input type="text" name="username" class="form_login" placeholder="Username .." required="required">
+ 
+			<label>Password</label>
+			<input type="password" name="password" class="form_login" placeholder="Password .." required="required">
+ 
+			<input type="submit" class="tombol_login" value="LOGIN">
+ 
+			<br/>
+			<br/>
+			<center>
+				<a class="link" href="https://www.malasngoding.com">kembali</a>
+			</center>
+		</form>
+		
+	</div>
+ 
+ 
 </body>
 </html>
